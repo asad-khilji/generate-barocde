116c2545431121eff1d93866fe9c337acd665403
asad-khilji <100327772+asad-khilji@users.noreply.github.com>
2024-01-26 11:20:39 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("style-size").innerHTML = prompt("Enter a style number and size");
+document.getElementById("category").innerHTML = prompt("Enter a category name");
+document.getElementById("upc").innerHTML = prompt("Enter a upc number");
